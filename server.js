226fbb26d5b0e98e011ab95b599e81e724ad7e5d
Ziyi Zhu <ziyizhu@ip-192-168-1-102.eu-west-2.compute.internal>
2024-05-07 01:04:29 +0100

    Fix static files

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(1337, () => {
